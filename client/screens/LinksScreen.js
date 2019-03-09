24516fc01ccaf1bed1e7d2ff358987523b6547ea
Bianca N <bianca.neptune@gmail.com>
2019-03-09 10:44:27 -0500

    basic map

--- client/screens/LinksScreen.js
+++ client/screens/LinksScreen.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React from "react";
+import { MapView } from "expo";
+import { Callout } from "react-native-maps";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: "Links"
   };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      >
+        <MapView.Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+        >
+          <Callout>
+            <View style={styles.calloutView}>
+              <Text>You are here</Text>
+            </View>
+          </Callout>
+        </MapView.Marker>
+      </MapView>
     );
   }
 }
@@ -22,6 +37,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
+  map: {
+    flex: 1
+  },
+  calloutView: {
+    flexDirection: "row",
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    borderRadius: 10,
+    width: "50%",
+    marginLeft: "30%",
+    marginRight: "30%",
+    marginTop: 20
+  }
 });
